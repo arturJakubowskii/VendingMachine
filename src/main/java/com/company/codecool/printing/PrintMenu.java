191a51771b454519a4cb463820b5895ba0c46e23
Artur Jakubowski <a.jakubowskipl@gmail.com>
2020-11-24 13:13:16 +0100

    Refactored menu

--- src/main/java/com/company/codecool/printing/PrintMenu.java
+++ src/main/java/com/company/codecool/printing/PrintMenu.java
@@ -4,12 +4,17 @@ public class PrintMenu {
 
     public static void print(){
         System.out.println(
-                "Basic food vending machine" + "\n"
-                        + "Our products:" + "\n"
-                        + "Sandwich: 3zl" + "\n"
-                        + "Cola: 1.50zl" + "\n"
-                        + "Candy: bar 1zl" + "\n"
-                        + "Water: 80gr" + "\n"
+                "Food vending machine" + "\n" + "\n"
+                        + "Our products:  "
+                        + "Sandwich: 3zl  Cola: 1.50zl  Candy: bar 1zl  Water: 80gr" + "\n" + "\n"
+                        + "Your Coins, enter option for coin input:" + "\n" + "\n"
+                        + "1. 10gr" +"\n"
+                        + "2. 20gr" +"\n"
+                        + "3. 50gr" +"\n"
+                        + "4. 1zl" +"\n"
+                        + "5. 2zl" +"\n"
+                        + "6. 5zl" +"\n"
+
         );
     }
 }
